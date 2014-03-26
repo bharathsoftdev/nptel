f97a3cc389542009257162942f4e33776eef028b
Bharath S <bharathsoftdev@users.noreply.github.com>
2014-03-26 21:15:58 +0530

    Update w2 - Print Elements of a Matrix in Spiral Order.c
    Fully functional Program

--- w2 - Print Elements of a Matrix in Spiral Order.c
+++ w2 - Print Elements of a Matrix in Spiral Order.c
@@ -21,33 +21,57 @@ CONSTRAINTS:
 
 int main()
 {
-/*write your code here*/
+	#include<stdio.h>
 
-	int m , n ;
-	
-	scanf ( "%d", &m);
-	scanf ( "%d", &n);
-	
-	int i, j ;
-	
-	int arr[m][n] ;
-	
-	for ( i = 0 ; i < m ; ++ i )
+int main()
+{
+
+	int m, n, i, k = 0, l = 0;
+	scanf ("%d%d",&m,&n);
+	int mat[m][n];
+	for ( k=0 ; k < m ; ++k )
 	{
-		for ( j = 0 ; j < n ; ++j )
+		for ( l=0 ; l < n ; ++l )
 		{
-			scanf ( "%d", &arr[i][j] );
+			scanf (" %d",&mat[k][l]);
 		}
 	}
-
-	
-	for ( i = 0 ; i < m ; ++i )
-	{
-		for ( j = 0 ; j < n ; ++j )
-		{
-			printf ( "%d", arr[i][j] );
-		}
-	}
-
+	printf ( "%d",mat[0][0]);
+	 k = 0;
+	 l = 0 ;
+	while (k < m && l < n)
+    	{
+	        for (i = l; i < n; ++i)
+	        {
+	        	if (( k == 0 ) && ( i == 0 ))
+	        		continue ;
+	            printf(" %d", mat[k][i]);
+	        }
+	        k++;
+	 
+	        for (i = k; i < m; ++i)
+	        {
+	            printf(" %d", mat[i][n-1]);
+	        }
+	        n--;
+	        if ( k < m)
+	        {
+	            for (i = n-1; i >= l; --i)
+        	    {
+	                printf(" %d", mat[m-1][i]);
+	            }
+        	    m--;
+	        }
+	        if (l < n)
+	        {
+	            for (i = m-1; i >= k; --i)
+	            {
+	                printf(" %d", mat[i][l]);
+	            }
+		    l++;   
+        	}       
+    	}
+    	return 0;
+}
     return 0;
 }
